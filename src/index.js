f833e75a0dfe47a3c47a2312b4ff96638477c766
Chau Duong <chau.duong1192@gmail.com>
2017-05-23 23:16:03 +0700

    fix(Issue #6): Import bootstrap.css before index.css

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
