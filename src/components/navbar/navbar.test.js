00c54aca878aae04d241c50831f0ca42796c9f08
Chau Duong <chau.duong1192@gmail.com>
2017-05-28 14:09:21 +0700

    revert(Close #4): remove components

--- src/components/navbar/navbar.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './navbar';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Navbar />, div);
-});
