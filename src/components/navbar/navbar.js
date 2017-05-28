30ec6d8d80ddd228b1ca17d1c51463a90cc7d00a
Chau Duong <chau.duong1192@gmail.com>
2017-05-28 13:58:56 +0700

    docs(Close issue #4): Rename image files.

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './navbar.scss';
+
+class Navbar extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="Navbar"></div>
+    );
+  }
+}
+
+export default Navbar;
