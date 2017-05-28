1f5243a0dc2e3af35cd8b5fdbe5d34ba5fbd85a4
Chau Duong <chau.duong1192@gmail.com>
2017-05-28 14:35:16 +0700

    feat(Issue #11): Genarate core components

---
+++ src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './footer.scss';
+
+class Footer extends Component {
+  state = {};
+
+  render() {
+    return (
+	    <footer>
+	       <p>© Gorillab 2017</p>
+	    </footer>
+    );
+  }
+}
+
+export default Footer;
