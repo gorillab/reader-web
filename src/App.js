2342bd41d6bc142c3d6f35bebc7280a3d7ee1ec5
Chau Duong <chau.duong1192@gmail.com>
2017-06-11 17:59:33 +0700

    feat(Close #14): add for you component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import ForYou from './components/ForYou';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
             <div className="page-content">
               <Route exact path="/" component={Home}/>
               <Route path="/:source" component={Home}/>
+              <Route path="/for-you" component={ForYou}/>
             </div>
             <Footer />
           </div>
---
+++ src/components/ForYou/ForYou.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ForYou.scss';
+
+class ForYou extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="ForYou"></div>
+    );
+  }
+}
+
+export default ForYou;
