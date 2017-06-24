fbaec70e7ada74c7a05d0c78f82ea3f8f9db7d5f
phatpham9 <phatpham9@gmail.com>
2017-06-24 18:40:31 +0700

    refactor: remove all uppercase files

--- src/components/PostList/PostList.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PostList from './PostList';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PostList />, div);
-});
--- src/components/Sort/Sort.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Sort from './Sort';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Sort />, div);
-});
