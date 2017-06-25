13299979064d5218b19f0d605291dfdadd8c2ee6
phatpham9 <phatpham9@gmail.com>
2017-06-26 00:19:51 +0700

    build(Close #43): upgrade to webpack 3

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -273,6 +273,8 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // Enable scope hoisting
+    new webpack.optimize.ModuleConcatenationPlugin(),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
