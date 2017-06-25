df44513ad990dd7c78ccbaec0896f5091a8fb714
phatpham9 <phatpham9@gmail.com>
2017-06-25 22:53:38 +0700

    refactor: rename components to PascalCase

--- src/common/content/content.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Content from './content';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Content />, div);
-});
--- src/common/page-title/page-title.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PageTitle from './page-title';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PageTitle />, div);
-});
