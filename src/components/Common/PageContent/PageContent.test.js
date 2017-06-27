f5eb4b396ca165fbdd2980b38442c26686ca2217
phatpham9 <phatpham9@gmail.com>
2017-06-27 20:04:24 +0700

    test: fix missing required children

--- src/components/Common/PageContent/PageContent.test.js
+++ src/components/Common/PageContent/PageContent.test.js
@@ -5,5 +5,5 @@ import PageContent from './PageContent';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<PageContent />, div);
+  ReactDOM.render(<PageContent>Test</PageContent>, div);
 });
