82f7c3e7999668812c7426c7e10a8955963e3474
Chau Duong <chau.duong1192@gmail.com>
2017-06-27 21:29:05 +0700

    test: update test

--- src/components/Common/Sort/Sort.test.js
+++ src/components/Common/Sort/Sort.test.js
@@ -8,6 +8,6 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <MemoryRouter>
-      <Sort />
+      <Sort current="new" getPosts={() => {}} />
     </MemoryRouter>, div);
 });
