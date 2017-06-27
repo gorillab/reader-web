956d8948cb35895e8e997421b708442606b5837b
phatpham9 <phatpham9@gmail.com>
2017-06-27 22:08:53 +0700

    refactor: print sort items repeatedly

--- src/components/Common/Sort/Sort.js
+++ src/components/Common/Sort/Sort.js
@@ -9,47 +9,36 @@ const propTypes = {
   getPosts: PropTypes.func.isRequired,
 };
 
-const Sort = ({ current, getPosts }) => (
-  <div className="sort">
-    <ul>
-      <li className={current === 'new' ? 'sort-active' : ''}>
-        <Link
-          id="new"
-          to={{
-            search: '?sort=new',
-          }}
-          onClick={getPosts}
-        >
-          New
-        </Link>
-      </li>
+const Sort = ({ current, getPosts }) => {
+  const items = [{
+    display: 'New',
+    value: 'new',
+  }, {
+    display: 'Best',
+    value: 'best',
+  }, {
+    display: 'Daily',
+    value: 'daily',
+  }];
 
-      <li className={current === 'best' ? 'sort-active' : ''}>
-        <Link
-          id="best"
-          to={{
-            search: '?sort=best',
-          }}
-          onClick={getPosts}
-        >
-          Best
-        </Link>
-      </li>
-
-      <li className={current === 'daily' ? 'sort-active' : ''}>
-        <Link
-          id="daily"
-          to={{
-            search: '?sort=daily',
-          }}
-          onClick={getPosts}
-        >
-          Daily
-        </Link>
-      </li>
-    </ul>
-  </div>
-);
+  return (
+    <div className="sort">
+      <ul>
+        {items.map(({ display, value }) => (
+          <li className={current === value ? 'sort-active' : ''}>
+            <Link
+              id={value}
+              to={{
+                search: `?sort=${value}`,
+              }}
+              onClick={getPosts}
+            >{display}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 Sort.propTypes = propTypes;
 
