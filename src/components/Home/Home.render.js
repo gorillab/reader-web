2e9b34217084742f37f9f6c0c8152d6864b0f306
phatpham9 <phatpham9@gmail.com>
2017-08-19 17:09:09 +0700

    refactor(Close #52): update home component & look up source title from state

---
+++ src/components/Home/Home.render.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Home from './Home';
+
+import { getSource as getSourceFunc } from '../../state/ducks/sources';
+
+const propTypes = {
+  match: PropTypes.any.isRequired,
+  getSource: PropTypes.func.isRequired,
+};
+
+const HomeRender = ({ match, getSource, ...rest }) => {
+  const source = match.params.source ? getSource(match.params.source) : null;
+  const title = source ? source.title : 'Explore';
+
+  return <Home title={title} source={source ? source.id : undefined} {...rest} />;
+};
+
+HomeRender.propTypes = propTypes;
+
+const FinalHomeRender = (props) => {
+  const ConnectedHomeRender = connect(
+    state => ({
+      getSource: getSourceFunc(state),
+    }),
+  )(HomeRender);
+
+  return <ConnectedHomeRender {...props} />;
+};
+
+export default FinalHomeRender;
