39e33f729ff4f5c1c14cf460f1526a923fa4f9e9
phatpham9 <phatpham9@gmail.com>
2017-08-20 00:52:27 +0700

    refactor: remove mock-data

--- src/components/Posts/PostsList.test.js
+++ src/components/Posts/PostsList.test.js
@@ -3,9 +3,44 @@ import ReactDOM from 'react-dom';
 
 import PostsList from './PostsList';
 
-import posts from '../../mock-data/posts';
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  const posts = [
+    {
+      id: 1,
+      thumbnail: '/images/logo.png',
+      title: 'Jean E. Sammet, who developed the FORMAC programming language, has died',
+      url: 'abc/abc',
+      by: {
+        name: 'tinhte.vn',
+        link: 'http://www.tinhte.vn',
+      },
+      time: Date.now(),
+      isRead: true,
+    },
+    {
+      id: 2,
+      thumbnail: '/images/logo.png',
+      title: 'Jean E. Sammet, who developed the FORMAC programming language, has died',
+      url: 'abc/abc',
+      by: {
+        name: 'tinhte.vn',
+        link: 'http://www.tinhte.vn',
+      },
+      time: Date.now(),
+    },
+    {
+      id: 3,
+      thumbnail: '/images/logo.png',
+      title: 'Jean E. Sammet, who developed the FORMAC programming language, has died',
+      url: 'abc/abc',
+      by: {
+        name: 'tinhte.vn',
+        link: 'http://www.tinhte.vn',
+      },
+      time: Date.now(),
+    },
+  ];
+
   ReactDOM.render(<PostsList posts={posts} />, div);
 });
