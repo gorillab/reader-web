e1462a9b21703ab15464cd2fa6a7863d4a453b76
phatpham9 <phatpham9@gmail.com>
2017-08-19 12:41:01 +0700

    test(Close #59): revert app test

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import configureStore from './state';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App store={configureStore(window.REDUX_INITIAL_DATA)} />, div);
+  ReactDOM.render(<App />, div);
 });
