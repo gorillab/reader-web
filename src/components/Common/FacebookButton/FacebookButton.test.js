b99cc95493c0b33acbac5fe7705ea02a940e2820
Chau Duong <chau.duong1192@gmail.com>
2017-09-17 17:20:52 +0700

    fix: fix test file

--- src/components/Common/FacebookButton/FacebookButton.test.js
+++ src/components/Common/FacebookButton/FacebookButton.test.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import FacebookButton from './FacebookButton';
 
 it('renders without crashing', () => {
+  const props = {
+    content: '',
+    classname: '',
+    action: () => {},
+  };
   const div = document.createElement('div');
-  ReactDOM.render(<FacebookButton />, div);
+  ReactDOM.render(<FacebookButton {...props} />, div);
 });
