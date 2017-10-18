cd7d41a7263138d8fe9253b267a2eaf76e7e7418
Chau Duong <chau.duong1192@gmail.com>
2017-10-18 11:10:53 +0700

    docs: change ui of navbar

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Auth } from '@gorillab/reader-js';
 
-import { Collapse, Navbar, Nav, NavItem, NavbarToggler, NavDropdown, DropdownToggle, DropdownMenu } from 'reactstrap';
+import { Collapse, Navbar, Nav, NavItem, NavDropdown, DropdownToggle, DropdownMenu } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 import { logOut, userSelectors } from '../../../state/ducks/user';
@@ -32,13 +32,40 @@ export class Header extends Component {
       navbarOpen: false,
       exploreDropdownOpen: false,
       userDropdownOpen: false,
+      exploreOpen: false,
+      userOpen: false,
+      isChecked: false,
     };
 
     this.toggle = this.toggle.bind(this);
     this.toggleExplore = this.toggleExplore.bind(this);
     this.toggleUser = this.toggleUser.bind(this);
+    this.openExplore = this.openExplore.bind(this);
+    this.openUser = this.openUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.unCheck = this.unCheck.bind(this);
   }
 
+  unCheck() {
+    this.setState({
+      isChecked: false,
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      isChecked: e.target.checked,
+    });
+  }
+  openExplore() {
+    this.setState({
+      exploreOpen: !this.state.exploreOpen,
+    });
+  }
+  openUser() {
+    this.setState({
+      userOpen: !this.state.userOpen,
+    });
+  }
   toggle() {
     this.setState({
       navbarOpen: !this.state.navbarOpen,
@@ -64,7 +91,72 @@ export class Header extends Component {
           <Navbar light toggleable>
             <Logo />
 
-            <NavbarToggler right onClick={this.toggle} />
+            {/* <NavbarToggler right onClick={this.toggle} /> */}
+            <div className="outer-menu">
+              <input
+                className="checkbox-toggle"
+                type="checkbox"
+                checked={this.state.isChecked}
+                onChange={this.handleChange}
+              />
+              <div className="hamburger">
+                <div>{}</div>
+              </div>
+              <div className="overlay overlay-hugeinc">
+                <nav>
+                  <ul>
+                    {!!this.props.sources.length &&
+                      <li>
+                        <a role="button" tabIndex={0} className={`active ${this.state.exploreOpen ? 'has-sub' : ''}`} onClick={this.openExplore}>
+                          <span>Explore</span>
+                          <ins>
+                            <i>{}</i>
+                          </ins>
+                        </a>
+                        { this.props.sources.map.length &&
+                          <ul className="sub">
+                            {this.props.sources.map(source => (
+                              <li key={source.id}>
+                                <Link to={`/source/${source.id}`} onClick={this.unCheck} key={source.id} id={source.id}>{source.title}</Link>
+                              </li>
+                          ))}
+                          </ul>
+                        }
+                      </li>
+                    }
+
+                    {this.props.isLoggedIn &&
+                      <li>
+                        <Link onClick={this.unCheck} to="/for-you">For You</Link>
+                      </li>
+                    }
+                    {this.props.isLoggedIn &&
+                      <li>
+                        <Link onClick={this.unCheck} to="/saved">Saved</Link>
+                      </li>
+                    }
+                    {!this.props.isLoggedIn ? (
+                      <li>
+                        <a
+                          role="button"
+                          tabIndex={0}
+                          onClick={() => { location.href = Auth.LOGIN_BY_FACEBOOK_URL; }}
+                        >
+                          Facebook
+                        </a>
+                      </li>
+                    ) : (
+                      <li>
+                        <a role="button" tabIndex={0} onClick={this.props.logOut}>
+                          Logout
+                        </a>
+                      </li>
+                    )}
+                  </ul>
+                </nav>
+                <div className="copyright">© Gorillab {new Date().getFullYear()}</div>
+              </div>
+            </div>
 
             <Collapse isOpen={this.state.navbarOpen} navbar>
               <Nav className="mr-auto nav" navbar>
