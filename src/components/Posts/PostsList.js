24b6666705cb243f4d67b68319ab6115f3b53be8
phatpham9 <phatpham9@gmail.com>
2017-10-24 22:49:05 +0700

    style: fix posts list width

--- src/components/Posts/PostsList.js
+++ src/components/Posts/PostsList.js
@@ -22,7 +22,7 @@ export const PostsList = ({ posts, getMore }) => (
       ))}
     </ul>
 
-    {getMore && <div className="text-center">
+    {getMore && <div className="get-more-wrapper">
       <button type="button" name="get-more" className="btn btn-get-more" onClick={getMore}>
         Read More
       </button>
