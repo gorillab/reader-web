b51636f79bd40f9a0b0fee222e49c1a7510c0a76
phatpham9 <phatpham9@gmail.com>
2018-03-11 01:28:58 +0700

    fix(close #108 #106): cache imags, favicon & manifest, cache ignore main files, not fallback for the above files

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -241,8 +241,8 @@ module.exports = {
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
-      filename: '[name].[id].[contenthash:8].css',
-      allChunks: true
+      filename: 'static/css/[name].[id].[contenthash:8].css',
+      allChunks: true,
     }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
@@ -268,8 +268,18 @@ module.exports = {
       },
       minify: true,
       navigateFallback: publicUrl + '/index.html',
-      navigateFallbackWhitelist: [/^(?!\/(__|api|admin)).*/],
-      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+      navigateFallbackWhitelist: [/^(?!\/(__|images|favicon|manifest|api|admin)).*/],
+      staticFileGlobs: [
+        paths.appBuild + '/images/*.*',
+        paths.appBuild + '/favicon.*',
+        paths.appBuild + '/manifest.json',
+      ],
+      staticFileGlobsIgnorePatterns: [
+        /\.map$/,
+        /asset-manifest\.json$/,
+        /main\./,
+      ],
+      mergeStaticsConfig: true,
       // Work around Windows path issue in SWPrecacheWebpackPlugin:
       // https://github.com/facebookincubator/create-react-app/issues/2235
       stripPrefix: paths.appBuild.replace(/\\/g, '/') + '/',
